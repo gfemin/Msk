db1eeb3b013f0c16703b3508b7c3c2642264985f
gfemin <minthantshin.mts11@gmail.com>
2026-01-03 10:51:08 +0630

    Update main.py

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ def run_checker(message):
                 
                 is_hit = 'Donation Successful!' in last or 'funds' in last or 'security code' in last
                 
-                if is_hit or (dd % 10 == 0):
+                if is_hit or (dd % 15 == 0):
                     try:
                         bot.edit_message_text(chat_id=chat_id, message_id=ko, text=view_text, reply_markup=markup)
                     except Exception as e:
